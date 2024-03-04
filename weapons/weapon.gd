05968cc879c201d534b13211c78db5178fc6d7aa
The Hue Man <thehueman21@gmail.com>
2024-03-03 20:09:23 -0800

    changed weapon scene to knife scene. weapon.gd will no longer be directly attached to any node

--- weapons/weapon.gd
+++ weapons/weapon.gd
@@ -29,13 +29,9 @@ func attack(attack_origin:Marker2D,target:Vector2,area:float):
 	
 	var new_attack = attack_scene.instantiate()
 	new_attack.position = weapon_origin.position
-	new_attack.look_at(target)
 	
 	new_attack.area = area
 	
 	get_parent().add_child(new_attack)
 
-
-func _on_cooldown_timeout():
-	attack(weapon_origin,get_local_mouse_position(),area)
 
